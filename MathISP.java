9f5a1f5ca5c66f6538bd203a3ee7877919818b12
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2023-01-05 12:45:55 -0500

    Ishan Code Fixed
    made it compile and added the packages for needed for random

--- MathISP.java
+++ MathISP.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 import javax.swing.*;
 
 public class MathISP extends JFrame implements ActionListener {
@@ -28,5 +30,66 @@ public class MathISP extends JFrame implements ActionListener {
 
     // Generate a new question
     generateQuestion();
+  }
+  private void generateQuestion() {
+    Random rand = new Random();
+
+    // Generate random integral values for x and y
+    int x = rand.nextInt(10) + 1;
+    int y = rand.nextInt(10) + 1;
+
+    // Generate a random operator
+    String[] operators = {"+", "-", "*", "/"};
+    String operator = operators[rand.nextInt(operators.length)];
+
+    // Set the question text
+    questionLabel.setText(String.format("What is %d %s %d?", x, operator, y));
+
+    // Store the correct answer in the check button's client property
+    int correctAnswer;
+    switch (operator) {
+      case "+":
+        correctAnswer = x + y;
+        break;
+      case "-":
+        correctAnswer = x - y;
+        break;
+      case "*":
+        correctAnswer = x * y;
+        break;
+      case "/":
+        correctAnswer = x / y;
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid operator: " + operator);
+    }
+    checkButton.putClientProperty("correctAnswer", correctAnswer);
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    // Get the user's answer
+    int answer = Integer.parseInt(answerField.getText());
+
+    // Get the correct answer from the check button's client property
+    int correctAnswer = (int) checkButton.getClientProperty("correctAnswer");
+
+    if (answer == correctAnswer) {
+      resultLabel.setText("Correct!");
+    } else {
+      resultLabel.setText("Incorrect. The correct answer is " + correctAnswer);
+    }
+    answerField.setText("");
+    generateQuestion();
+  }
+  
+
+  public static void main(String[] args) {
+    MathISP frame = new MathISP();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 150);
+    frame.setBackground(Color.BLUE);
+    frame.setVisible(true);
+    
   }
 }
